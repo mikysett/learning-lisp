81a225c747314cc669d8ee085d9a01bd7be28ea6
mikysett <mikysett@gmail.com>
2021-07-25 23:47:22 +0100

    unit test chap of gigamonkeys.com finished

--- unit-tests.lisp
+++ unit-tests.lisp
@@ -1,20 +1,22 @@
+(defvar *test-name* nil)
+
 (defmacro with-gensym ((&rest names) &body body)
   `(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
 
-(defun test-+ ()
+(defun first-test-+ ()
   (and
    (= (+ 1 2) 3)
    (= (+ 1 2 3) 6)
    (= (+ -1 -3) -4)))
 
-(defun test2-+ ()
+(defun second-test-+ ()
   (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
   (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
   (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
   result)
 
 (defmacro combine-results (&body forms)
@@ -27,8 +29,23 @@
   `(combine-results
      ,@(loop for f in forms collect `(report-result ,f ',f))))
 
-(defun test3-+ ()
-  (check
-    (= (+ 1 2) 3)
-    (= (+ 1 2 3) 6)
-    (= (+ -1 -3) -4)))
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+(deftest test-+ ()
+    (Check
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3) -4)))
+
+(deftest test* ()
+    (check
+      (= (* 3 4) 12)
+      (= (* 7 -2) -14)))
+
+(deftest test-arithmetics ()
+  (combine-results
+    (test-+)
+    (test*)))
